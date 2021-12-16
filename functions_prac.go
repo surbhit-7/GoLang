44007243806b1f625b6bc4c6463fcaecab046550
Surbhit Vishwakarma <surbhitvishwakarma7@gmail.com>
2021-12-16 17:14:08 +0530

    adding new files

--- functions_prac.go
+++ functions_prac.go
@@ -4,26 +4,26 @@ import "fmt"
 
 // FUNC 1
 // CALL BY VALUE
-func add(x, y int) int {
+func adds(x, y int) int {
 	z := x + y
 	return z
 }
 
 // CAll By reference
-func swep(x, y *int) {
+func sw(x, y *int) {
 	a := *x
 	*x = *y
 	*y = a
 }
 func main() {
-	var a, b (int)
+	var a, b int
 	fmt.Scanln(&a, &b)
-	fmt.Println(add(a, b))
+	fmt.Println(adds(a, b))
 
 	k := 2
 	l := 3
 	fmt.Println("Before Swaps : ", k, l)
 
-	swep(&k, &l)
+	sw(&k, &l)
 	fmt.Println("after Swaps : ", k, l)
 }
