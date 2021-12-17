909072b5c6fd5fb82e58b628ce99d4336eaa44f0
Surbhit Vishwakarma <surbhitvishwakarma7@gmail.com>
2021-12-17 19:43:57 +0530

    Added Pointer

--- structs/embedded_struct_prac.go
+++ structs/embedded_struct_prac.go
@@ -18,8 +18,13 @@ func main() {
 	boy.lastName = "Vishwakarma"
 	boy.contactInfo.zipcode = 482002
 	boy.contactInfo.phoneNumber = "9589228632"
+	boyPointer := &boy
+	boyPointer.updateName("ShaggY")
 	boy.print()
 }
 func (p person) print() {
 	log.Println(p)
+}
+func (updateNamePtr *person) updateName(name string) {
+	updateNamePtr.firstName = name
 }
