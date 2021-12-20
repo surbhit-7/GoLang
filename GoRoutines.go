b7cc964a38061bccd3c041d232a53115f5aa533e
Surbhit Vishwakarma <surbhitvishwakarma7@gmail.com>
2021-12-20 16:00:27 +0530

    learned about goroutine and defer

--- GoRoutines.go
+++ GoRoutines.go
@@ -8,13 +8,24 @@ import (
 
 var wg sync.WaitGroup
 
-func say(s string) {
+func helper() {
+	wg.Done()
+	if r := recover(); r != nil {
+		log.Println("Recovered in cleanup")
+	}
+}
 
+func say(s string) {
+	defer helper()
 	for i := 1; i < 5; i++ {
 		log.Println("say :", s)
 		time.Sleep(time.Millisecond * 100)
+
+		if i == 2 {
+			panic("oh shit")
+		}
 	}
-	wg.Done()
+
 }
 
 func main() {
