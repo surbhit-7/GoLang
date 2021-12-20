5faa026097c41896a9f1a936a3718409dc4b100b
Surbhit Vishwakarma <surbhitvishwakarma7@gmail.com>
2021-12-20 18:02:39 +0530

    basics of interfaces

---
+++ interfaces_practise.go
@@ -0,0 +1,31 @@
+package main
+
+import "log"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+
+}
+func printGreeting(b bot) {
+	log.Println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "HELLO"
+}
+
+func (spanishBot) getGreeting() string {
+
+	return "OLA"
+}
